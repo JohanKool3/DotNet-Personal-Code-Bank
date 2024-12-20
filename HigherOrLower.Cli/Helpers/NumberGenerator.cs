a830f62568df9965b72da3cd20ebde46280a28d6
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2024-12-20 13:24:44 +0000

    Add project files.

---
+++ HigherOrLower.Cli/Helpers/NumberGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace HigherOrLower.Cli.Helpers
+{
+    /// <summary>
+    /// A Simple number generator
+    /// </summary>
+    internal class NumberGenerator
+    {
+        /// <summary>
+        /// Generate a number between a lower and upper bound
+        /// </summary>
+        /// <param name="lowerBound">The minimum Number (inclusive)</param>
+        /// <param name="upperBound">The maximum Number (exclusive)</param>
+        /// <returns></returns>
+        public static int Generate(int lowerBound, int upperBound)
+        {
+            var generator = new Random();
+
+            return generator.Next(lowerBound, upperBound);
+        }
+    }
+}
---
+++ HigherOrLower.Cli/Program.cs
@@ -0,0 +1,59 @@
+﻿using HigherOrLower.Cli.Helpers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Tracking Game State
+        var maxAttempts = 7;
+        var attempts = 0;
+        var gameWon = false;
+
+        var numberToGuess = NumberGenerator.Generate(0, 100);
+        Console.WriteLine("I am thinking of a number between 0 and 100");
+
+        while (attempts < maxAttempts && !gameWon)
+        {
+            Console.Write("Your Guess: ");
+            var userInput = Console.ReadLine();
+
+            var successful = int.TryParse(userInput, out int userGuess);
+
+            if (!successful)
+            {
+                Console.WriteLine("Invalid Input");
+                continue;
+            }
+
+            // Check the number
+            if (userGuess == numberToGuess)
+            {
+                attempts++;
+                gameWon = true;
+            }
+            else if (userGuess > numberToGuess)
+            {
+                attempts++;
+                Console.WriteLine($"({attempts}/{maxAttempts}) You Guessed Higher");
+
+            }
+            else
+            {
+                attempts++;
+                Console.WriteLine($"({attempts}/{maxAttempts}) You Guessed Lower");
+
+            }
+        }
+
+        if (gameWon)
+        {
+            Console.WriteLine("You Guessed Correctly! You Win");
+            Console.WriteLine($"Total Guesses {attempts} / {maxAttempts}");
+        }
+        else
+        {
+            Console.WriteLine("You ran out of guesses! You Lose");
+            Console.WriteLine($"Total Guesses {attempts} / {maxAttempts}");
+        }
+    }
+}
